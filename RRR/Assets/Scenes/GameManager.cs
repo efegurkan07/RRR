5c233d65e10de16efbff499be96a70094011eb74
Marco Behnke <marco.behnke@innogames.com>
2020-01-31 23:05:29 +0100

    base scene setup

---
+++ RRR/Assets/Scenes/GameManager.cs
@@ -0,0 +1,23 @@
+﻿
+public class GameManager
+{
+    private static GameManager _instance;
+    
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    public void AnyMethod(int property)
+    {
+        
+    }
+}
