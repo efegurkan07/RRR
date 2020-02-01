53284e6d47f1e4a184b67804c59710bc63d4158c
Marco Behnke <marco.behnke@innogames.com>
2020-02-01 01:36:52 +0100

    base window flow

---
+++ RRR/Assets/Scripts/CreditsHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsHandler : MonoBehaviour
+{
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+    }
+}
--- RRR/Assets/Scripts/MainMenuHandler.cs
+++ RRR/Assets/Scripts/MainMenuHandler.cs
@@ -9,4 +9,14 @@ public class MainMenuHandler : MonoBehaviour
     {
         SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
     }
+    
+    public void GotoCredits()
+    {
+        SceneManager.LoadSceneAsync("Credits", LoadSceneMode.Single);
+    }
+    
+    public void GotoHighscore()
+    {
+        SceneManager.LoadSceneAsync("Highscore", LoadSceneMode.Single);
+    }
 }
