bbdc8e10f9c235d36c1db677ce96d3ab967e1fc1
Marco Behnke <marco.behnke@innogames.com>
2020-02-01 08:58:50 +0100

    timer and game over

---
+++ RRR/Assets/Scripts/GameOverHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+    }
+}
