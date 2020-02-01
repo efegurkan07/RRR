a7d7757189e29786300840e2da9d400ad6d0d883
Aaron-Jonas Kuehne <aaron-jonas.kuehne@innogames.com>
2020-02-01 21:28:16 +0100

    wilhelscream and skyline

--- RRR/Assets/Scripts/Config.cs
+++ RRR/Assets/Scripts/Config.cs
@@ -2,7 +2,7 @@ public static class Config
 {
 	public const float rightLineLimit = 20;
 	public const float levelRunSpeed = 3f;
-	public static float skylineRunSpeed = 0.15f;
+	public static float skylineRunSpeed = 0.05f;
 	public static float maximumHealth = 110f;
 	public const int bodyPartCount = 3;
 	public const int initialBodyPartHealth = 100;
