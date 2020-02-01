6c707b15114a9255fe3026bc0e134af38f273b8e
Marco Behnke <marco.behnke@innogames.com>
2020-02-01 08:27:59 +0100

    clean up imports

--- RRR/Assets/Scripts/GameUIHandler.cs
+++ RRR/Assets/Scripts/GameUIHandler.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.SceneManagement;
 
 public class GameUIHandler : MonoBehaviour
