3aeaadbb6212bc834fe9359f413f8f52105eb087
Marco Behnke <marco.behnke@innogames.com>
2020-02-02 11:34:37 +0100

    play trash sound

--- RRR/Assets/Scripts/Obstacle.cs
+++ RRR/Assets/Scripts/Obstacle.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
@@ -6,6 +7,13 @@ public class Obstacle : MonoBehaviour
 	[SerializeField] private int Score = 0;
 	[SerializeField] private SparePart.SparePartType SparePartHandOut = SparePart.SparePartType.EMPTY;
 	[SerializeField] private GameObject DeathFX;
+	
+	private AudioSource DeathSound;
+
+	private void Awake()
+	{
+		DeathSound = GetComponent<AudioSource>();
+	}
 
 	private void Start()
 	{
@@ -34,6 +42,13 @@ public class Obstacle : MonoBehaviour
 
 	public void OnObjectCollided()
 	{
+		if (DeathSound)
+		{
+			var playSound = GameObject.Find("PlaySound").GetComponent<AudioSource>();
+			playSound.clip = DeathSound.clip;
+			playSound.Play();
+		}
+		
 		if (DeathFX)
 		{
 			var fx = Instantiate(DeathFX);
