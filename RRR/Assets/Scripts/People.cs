ba8a32402c5f4612b418695cd5d5c2a6ae5b6221
Marco Behnke <marco.behnke@innogames.com>
2020-02-02 12:43:10 +0100

    more sounds for people

--- RRR/Assets/Scripts/People.cs
+++ RRR/Assets/Scripts/People.cs
@@ -1,14 +1,21 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
 {
-	
 	public class People : MonoBehaviour
 	{
+		[SerializeField] AudioClip[] TriggerSounds;
+
 		private void Start()
 		{
 			GetComponent<Animator>().SetTrigger("HeavensCalling");
+			var audioPlayer = GetComponent<AudioSource>();
+
+			int randomIndex = Random.Range(0, TriggerSounds.Length);
+			audioPlayer.clip = TriggerSounds[randomIndex];
+			audioPlayer.Play();
 		}
 	}
 }
