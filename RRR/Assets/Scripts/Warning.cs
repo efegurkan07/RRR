cf512e1bef096b91cd698314d6d8b6b143717eaf
Marco Behnke <marco.behnke@innogames.com>
2020-02-02 12:19:19 +0100

    move scripts

---
+++ RRR/Assets/Scripts/Warning.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Warning : MonoBehaviour
+{
+	public TextMeshPro[] texts;
+
+	private void Update()
+	{
+		foreach (var t in texts)
+		{
+			t.outlineWidth = Mathf.Abs(Mathf.Sin(Mathf.Rad2Deg * Time.time / 10f)) * 0.3f;
+			t.color = new Color(0.5f + ((Mathf.Sin(Mathf.Rad2Deg * Time.time / 5f) + 1f) / 2f) * 0.5f, 0, 0);
+		}
+	}
+}
