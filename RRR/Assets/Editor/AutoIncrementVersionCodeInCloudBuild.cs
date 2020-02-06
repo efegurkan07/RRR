485ab6f23b486ead96f5cb92b86c04525711b3c8
Marco Behnke <marco.behnke@innogames.com>
2020-02-06 10:10:50 +0100

    cloud build auto increment version  no

---
+++ RRR/Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
+{
+#if UNITY_CLOUD_BUILD
+    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
+    {
+        string buildNumber = manifest.GetValue("buildNumber", "0");
+        Debug.LogWarning("Setting build number to " + buildNumber);
+        PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
+        PlayerSettings.iOS.buildNumber = buildNumber;
+    }
+#endif
+}
