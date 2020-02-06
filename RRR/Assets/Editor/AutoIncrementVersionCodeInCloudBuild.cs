47db30e8ce30a0fd9135bcdf70f6808bf798c28e
Marco Behnke <marco.behnke@innogames.com>
2020-02-06 13:26:31 +0100

    more output

--- RRR/Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
+++ RRR/Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -1,12 +1,21 @@
-﻿public class AutoIncrementVersionCodeInCloudBuild : UnityEngine.MonoBehaviour
+﻿using Boo.Lang.Runtime;
+
+public class AutoIncrementVersionCodeInCloudBuild : UnityEngine.MonoBehaviour
 {
-#if UNITY_CLOUD_BUILD
     public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
-        string buildNumber = manifest.GetValue("buildNumber", "0");
-        UnityEngine.Debug.LogWarning("Setting build number to " + buildNumber);
+        var buildNumber = manifest.GetValue<string>("buildNumber");
+        UnityEngine.Debug.Log("Setting build number to " + buildNumber);
+
+        if (buildNumber == null || buildNumber.Equals("") || buildNumber.Equals("0"))
+        {
+            UnityEngine.Debug.LogError("Could not retrieve a build number from the manifest.");
+            UnityEngine.Debug.LogError(manifest.ToString());
+            
+            throw new RuntimeException("Could not retrieve a build number from the manifest.");
+        }
+        
         UnityEditor.PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
         UnityEditor.PlayerSettings.iOS.buildNumber = buildNumber;
     }
-#endif
 }
