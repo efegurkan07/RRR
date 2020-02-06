e0efa6d272dc9fa11ce63fa8afbcc2d3ad339d83
Marco Behnke <marco.behnke@innogames.com>
2020-02-06 10:22:10 +0100

    missing imports

--- RRR/Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
+++ RRR/Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -1,14 +1,12 @@
-﻿using UnityEngine;
-
-public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
+﻿public class AutoIncrementVersionCodeInCloudBuild : UnityEngine.MonoBehaviour
 {
 #if UNITY_CLOUD_BUILD
     public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
         string buildNumber = manifest.GetValue("buildNumber", "0");
-        Debug.LogWarning("Setting build number to " + buildNumber);
-        PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
-        PlayerSettings.iOS.buildNumber = buildNumber;
+        UnityEngine.Debug.LogWarning("Setting build number to " + buildNumber);
+        UnityEditor.PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
+        UnityEditor.PlayerSettings.iOS.buildNumber = buildNumber;
     }
 #endif
 }
